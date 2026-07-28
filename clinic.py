ec2c84a77589d5c7009e8dd8fc50d768c269ec18
prashu46 <prashanthkvreddy@gmail.com>
2026-07-28 13:51:59 +0530

    updated patient login

--- clinic.py
+++ clinic.py
@@ -1,9 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/patient",methods=["GET","POST"])
+def patient():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        print("Email: ",email)
+        print("Password :",password)
+        
+    return render_template("patient_login.html")
+
+@app.route("/doctor")
+def doctor():
+    return render_template("doctor_login.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 app.run(debug=True)
