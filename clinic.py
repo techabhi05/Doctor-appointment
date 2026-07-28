4b1eedba05bc9534f1cc41da04a1bc0a84312eb9
prashu46 <prashanthkvreddy@gmail.com>
2026-07-28 15:27:00 +0530

    created registration desk

--- clinic.py
+++ clinic.py
@@ -1,7 +1,16 @@
 from flask import Flask,render_template,request
+import mysql.connector
 
 app = Flask(__name__)
 
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "prashu@46",
+    database = "clinic_management"
+)
+
+print("Database connected successfully")
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -12,9 +21,10 @@ def patient():
         email = request.form["email"]
         password = request.form["password"]
 
+
         print("Email: ",email)
         print("Password :",password)
-        
+
     return render_template("patient_login.html")
 
 @app.route("/doctor")
@@ -25,4 +35,18 @@ def doctor():
 def about():
     return render_template("about.html")
 
+@app.route("/patient/register", methods = ["GET","POST"])
+def patient_registers():
+        if request.method == "POST":
+            name = request.form["name"]
+            email = request.form["email"]
+            password = request.form["password"]
+
+            print("Name: ",name)
+            print("Email: ",email)
+            print("Password :",password)
+
+        return render_template("patient_register.html")
+
+    
 app.run(debug=True)
